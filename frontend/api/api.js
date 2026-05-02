91d55fa8f8735ba0e570e535e680935a062b046c
studentashu <ponnuashwini6@gmail.com>
2026-05-02 15:15:24 +0530

    fix frontend start script

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: //"http://localhost:8000"
+  "https://teammanagement-production-2074.up.railway.app"
 });
 
 // ADD TOKEN AUTOMATICALLY
